f843c913515df7817d80695c6a708d2a3697a8b3
Vadim Bykov <bvntaev@gmail.com>
2020-10-07 08:59:29 +0300

    feat:  solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = n.toString().split('').reverse().filter(item => item >= 0).join('')
+    return Number(res)
 }
